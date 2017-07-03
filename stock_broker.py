01ca63fbf8fc30004d9bcc5dc89720b16f067bd2
Akhil Kandimalla <kandmll2@illinois.edu>
2017-07-03 18:24:09 +0530

    Update stock_broker.py

--- stock_broker.py
+++ stock_broker.py
@@ -1,4 +1,5 @@
 import quandl
+import datetime
 
 path = "/Users/Akhil/anaconda/lib/python3.6/site-packages/quandl "
 auth_tok = "JMkgHX4qe3vKdhz8s46k"
@@ -8,9 +9,40 @@ print()
 name= input("Enter the name of the company: ")
 number_of_shares=input("Enter the number of shares:")
 company = "WIKI/"+name
+
+curr_date= datetime.date.today().strftime("20%y-%m-%d")
+print (curr_date)
 BUYDATE= input("Enter the BUY date in YYYY-MM-DD format ")
-SELLDATE= input("Enter the SELL date in YYYY-MM-DD format ")
+
+sale=input("Would you like to know the net difference if you sell at current price? (y/n)")
+if sale=="n":
+	SELLDATE= input("Enter the SELL date in YYYY-MM-DD format ")
+elif sale=="y":
+	
+	SELLDATE= datetime.date.today().strftime("20%y-%m-%d")
+	year, month, day = (int(x) for x in SELLDATE.split('-'))    
+	day = datetime.date(year, month, day).weekday()
+	
+
+	if day==0: 
+		day= int(SELLDATE[9:10])
+		day = day -3
+		
+		if day<10:
+			day="0"+str(day)
+		SELLDATE= SELLDATE[0:8]+str(day)
+	elif answer==6:
+		print("ggg")
+		day= int(SELLDATE[9:10])
+		day= day-1
+		if day<10:
+			day="0"+str(day)
+		SELLDATE= SELLDATE[0:8]+str(day)
+print("Selling date is: ",SELLDATE)
 price_type={'closing':".4", 'low':".3", 'high':".2", 'open':".1"}
+
+print("buydate: ", type(BUYDATE), "Selldate:  ", type(SELLDATE))
+print(BUYDATE, SELLDATE)
 
 key= input("Enter 'closing' for closing price, 'low' for low price, 'high' for high price, 'open' for opening price: ")
 
@@ -27,10 +59,12 @@ mydata2 = quandl.get(company+price_type[key], start_date=SELLDATE, end_date=SELL
 #mydata = quandl.get("WIKI/NVDA"+".4", start_date="2017-06-01",end_date="2017-06-27", transformation="diff")
 mydata1=mydata1.to_string()
 mydata2=mydata2.to_string()
-
+print (mydata1, mydata2)
 
 data1_val= mydata1[48:56]
 data2_val= mydata2[48:56]
+print("d1:  ",data1_val)
+print("d2:  ",data2_val)
 
 data1_val=float(data1_val)
 data2_val=float(data2_val)
@@ -53,6 +87,9 @@ print()
 #net_diff= mydata1['Adj. Close']-mydata2['Adj. Close']
 #rint("Net difference is: ",net_diff)
 
+
+
+		
 
 
 		
