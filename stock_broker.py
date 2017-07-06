03281e799d27ab26843336823bb6e68de6e40922
Akhil Kandimalla <kandmll2@illinois.edu>
2017-07-06 13:26:32 +0530

    Current price/current date fixed

--- stock_broker.py
+++ stock_broker.py
@@ -1,5 +1,5 @@
 import quandl
-import datetime
+from datetime import date, timedelta
 
 path = "/Users/Akhil/anaconda/lib/python3.6/site-packages/quandl "
 auth_tok = "JMkgHX4qe3vKdhz8s46k"
@@ -10,18 +10,18 @@ name= input("Enter the name of the company: ")
 number_of_shares=input("Enter the number of shares:")
 company = "WIKI/"+name
 
-curr_date= datetime.date.today().strftime("20%y-%m-%d")
+curr_date= date.today()
 print (curr_date)
 
 BUYDATE= input("Enter the BUY date in YYYY-MM-DD format ")
 year, month, day = (int(x) for x in BUYDATE.split('-'))    
-day = datetime.date(year, month, day).weekday()
+day = date(year, month, day).weekday()
 
 while(day==5 or day==6 or day==8):
 	print("The date you have entered is either a Saturday or a Sunday. Please enter a different date")
 	BUYDATE= input("Enter the BUY date in YYYY-MM-DD format ")
 	year, month, day = (int(x) for x in BUYDATE.split('-'))    
-	day = datetime.date(year, month, day).weekday()
+	day = date(year, month, day).weekday()
 
 
 
@@ -31,53 +31,58 @@ sale=sale.lower()
 if sale=="n":
 	SELLDATE= input("Enter the SELL date in YYYY-MM-DD format ")
 	year, month, day = (int(x) for x in SELLDATE.split('-'))    
-	day_sale = datetime.date(year, month, day).weekday()
+	day_sale = date(year, month, day).weekday()
 
 	while (day_sale==5 or day_sale==6):
 		print("The date you have entered is either a saturday or a sunday. Pls enter a different date")
 		SELLDATE= input("Enter the SELL date in YYYY-MM-DD format ")
 		year, month, day = (int(x) for x in SELLDATE.split('-'))    
-		day_sale = datetime.date(year, month, day).weekday()
+		day_sale = date(year, month, day).weekday()
 
 elif sale=="y":
 	
-	SELLDATE= datetime.date.today().strftime("20%y-%m-%d")
+	SELLDATE= date.today().strftime("20%y-%m-%d")
 	year, month, day = (int(x) for x in SELLDATE.split('-'))    
-	day = datetime.date(year, month, day).weekday()
+	day_of_week = date(year, month, day).weekday()
 	
 
-	if day==7: 
-		day= int(SELLDATE[9:10])
-		day = day -2
+	if day_of_week==7:
 		
-		if day<10:
-			day="0"+str(day)
+		SELLDATE= date.today()-timedelta(2)
+		SELLDATE= SELLDATE.strftime("20%y-%m-%d")
 		
-		SELLDATE= SELLDATE[0:8]+str(day)
 	
-	elif day==6:
-	
-		day= int(SELLDATE[9:10])
-		day= day-1
-		if day<10:
-			day="0"+str(day)
-		SELLDATE= SELLDATE[0:8]+str(day)
+	elif day_of_week==6:
+		SELLDATE= date.today()-timedelta(1)
+		SELLDATE= SELLDATE.strftime("20%y-%m-%d")
+		
+		
 #price_type={'closing':".4", 'low':".3", 'high':".2", 'open':".1"}
 
-print("buydate: ", BUYDATE, "Selldate:  ", SELLDATE)
 
 #key= input("Enter 'closing' for closing price, 'low' for low price, 'high' for high price, 'open' for opening price: ")
 
 #mydata = quandl.get(company+price_type[key], start_date="2017-06-15", end_date="2017-06-27")
 mydata1 = quandl.get(company+'.4', start_date=BUYDATE, end_date=BUYDATE)
 mydata2 = quandl.get(company+'.4', start_date=SELLDATE, end_date=SELLDATE)
+
+i=0
+while(mydata2.Close.dtype== object):
+	#year, month, day = (int(x) for x in SELLDATE.split(','))    
+	i+=1
+	SELLDATE= curr_date-timedelta(i)
+	SELLDATE= SELLDATE.strftime("20%y-%m-%d")
+	mydata2 = quandl.get(company+'.4', start_date=SELLDATE, end_date=SELLDATE)
+
+print("buydate: ", BUYDATE, "Selldate:  ", SELLDATE)
+
 #mydata1 = mydata1[['Close']]
 #mydata2= mydata2[['Close']]
 #mydata= [mydata1, mydata2]
 #difference =mydata[0]['Close']-mydata1[1]['Close']
 #mydata = quandl.get("WIKI/NVDA"+".4", start_date="2017-06-01",end_date="2017-06-27", transformation="diff")
-print("Type of raw data: ", type(mydata1))
-print ("TEST: ",mydata1.Close)
+#print("Type of raw data: ", type(mydata1))
+#print ("TEST: ",mydata1.Close)
 mydata1_str=mydata1.to_string()
 mydata2_str=mydata2.to_string()
 #print (mydata1, mydata2)
@@ -87,8 +92,7 @@ data2_val= mydata2_str[48:56]
 print("Bought at:  ",data1_val)
 print("Sold at:  ",data2_val)
 
-if data2_val=="   ":
-	print("IDGAF")
+
 
 data1_val=float(data1_val)
 data2_val=float(data2_val)
